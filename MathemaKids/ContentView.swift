7c04b61a61a03f55acee46738028aa879df69ef8
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-28 09:49:31 +0545

    Implement Basic UI

--- MathemaKids/ContentView.swift
+++ MathemaKids/ContentView.swift
@@ -7,15 +7,86 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GameSettingsView: View {
+    @State private var difficultyLevel = 5
+    @State private var questionsToAsk = 5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Stepper("Ask me \(questionsToAsk) questions", value: $questionsToAsk, in: 5...20, step: 5)
+        
+        Picker("From multiplication tables up to", selection: $difficultyLevel) {
+            ForEach(2..<13) { number in
+                Text(number.formatted())
+                    .tag(number)
+            }
         }
-        .padding()
+    }
+}
+
+struct GameView: View {
+    @State private var userAnswer = ""
+    @FocusState private var isAnswerFocused: Bool
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("12 x 12 = ?")
+                .font(.largeTitle.monospaced().bold())
+                .padding(.vertical, 80)
+            TextField("Enter Your Answer", text: $userAnswer)
+                .multilineTextAlignment(.center)
+                .focused($isAnswerFocused)
+                .keyboardType(.numberPad)
+        }
+        .toolbar {
+            if isAnswerFocused {
+                Button("Done") {
+                    isAnswerFocused = false
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var configuringGame = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section(configuringGame ? "Game Settings" : "") {
+                    if configuringGame {
+                        GameSettingsView()
+                    }
+                    playOrBackButtonView
+                }
+                
+                if !configuringGame {
+                    GameView()
+                }
+            }
+            .navigationTitle("MathemaKids")
+        }
+    }
+    
+    var playOrBackButtonView: some View {
+        Button {
+            withAnimation {
+                configuringGame.toggle()
+            }
+        } label: {
+            HStack {
+                if !configuringGame {
+                    Image(systemName: "chevron.backward")
+                }
+                
+                Text(configuringGame ? "Play" : "Back")
+                    .contentTransition(.numericText())
+                
+                if configuringGame {
+                    Image(systemName: "chevron.forward")
+                }
+            }
+        }
     }
 }
 
