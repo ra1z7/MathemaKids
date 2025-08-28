36f15b6d5bb6e493fe93be3cf5cd56192bcf7c38
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-28 08:07:10 +0545

    Initial Commit

---
+++ MathemaKids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathemaKids
+//
+//  Created by Purnaman Rai (College) on 28/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathemaKids/MathemaKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathemaKidsApp.swift
+//  MathemaKids
+//
+//  Created by Purnaman Rai (College) on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MathemaKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
