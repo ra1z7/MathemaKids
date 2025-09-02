04787d4230e5ad8784600560db2adfc0c2132a0b
ra1z <purnamaanrai7@gmail.com>
2025-09-02 09:51:53 +0545

    Make basic playful UI, added music

--- MathemaKids/ContentView.swift
+++ MathemaKids/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Purnaman Rai on 28/08/2025.
 //
 
+import AVFoundation
 import SwiftUI
 
 struct QuestionAnswer {
@@ -21,6 +22,172 @@ struct QuestionAnswer {
             questionAnswers.append(QuestionAnswer(question: question, answer: answer))
         }
         return questionAnswers
+    }
+}
+
+class AudioManager {
+    static var shared = AudioManager()
+    var player: AVAudioPlayer?
+
+    func playMusic() {
+        guard let url = Bundle.main.url(forResource: "Playful", withExtension: "mp3") else {
+            print("Playful.mp3 not found")
+            return
+        }
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.numberOfLoops = -1
+            player?.play()
+        } catch {
+            print("Could not play music: \(error.localizedDescription)")
+        }
+    }
+
+    func stopMusic() {
+        player?.stop()
+    }
+}
+
+struct BackgroundView: View {
+    let allBackgrounds: [ImageResource] = [
+        .backgroundCastles,
+        .backgroundColorDesert,
+        .backgroundColorFall,
+        .backgroundColorForest,
+        .backgroundColorGrass,
+        .backgroundDesert,
+        .backgroundForest
+    ]
+    
+    @State private var xAxis = 1300.0
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0 ..< 3) { _ in
+                Image(.backgroundColorGrass)
+            }
+        }
+        .scaledToFill()
+        .ignoresSafeArea()
+        .offset(x: xAxis, y: 0)
+        .onAppear {
+            AudioManager.shared.playMusic()
+            withAnimation(.linear(duration: 300)) {
+                xAxis = -1300.0
+            }
+        }
+    }
+}
+
+struct GameSettingsView: View {
+    @State private var questionsToAsk = 10
+    @State private var difficultyLevel = 5
+    
+    var body: some View {
+        ZStack {
+            BackgroundView()
+            
+            VStack {
+                Spacer()
+                Spacer()
+                
+                Text("MathemaKids")
+                    .font(.custom("Jua-Regular", size: 50))
+                    .foregroundStyle(.white)
+                    .shadow(radius: 1)
+                    .shadow(radius: 1)
+                    .shadow(radius: 10)
+                
+                Spacer()
+                
+                VStack {
+                    Text("Game Settings")
+                        .font(.custom("Jua-Regular", size: 22))
+                        .foregroundStyle(.secondary)
+                        .padding(.top, 30)
+                        .padding(.bottom, 20)
+                    
+                    HStack {
+                        Button {
+                            if questionsToAsk >= 10 {
+                                questionsToAsk -= 5
+                            }
+                        } label: {
+                            Image(.arrowLeft)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        Text("Ask me \(questionsToAsk) questions")
+                            .font(.custom("Jua-Regular", size: 20))
+                            .frame(width: 220)
+                        Button {
+                            if questionsToAsk <= 15 {
+                                questionsToAsk += 5
+                            }
+                        } label: {
+                            Image(.arrowRight)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                    }
+                    .padding()
+                
+                    
+                    HStack {
+                        Button {
+                            if difficultyLevel > 2 {
+                                difficultyLevel -= 1
+                            }
+                        } label: {
+                            Image(.arrowLeft)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        Text("From multiplication tables up to \(difficultyLevel)")
+                            .font(.custom("Jua-Regular", size: 20))
+                            .frame(width: 220)
+                            .multilineTextAlignment(.center)
+                        Button {
+                            if difficultyLevel < 12 {
+                                difficultyLevel += 1
+                            }
+                        } label: {
+                            Image(.arrowRight)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                    }
+                    .padding()
+                    .padding(.bottom, 30)
+                }
+                .padding(.horizontal, 10)
+                .background(.white)
+                .clipShape(.rect(cornerRadius: 20))
+                .shadow(radius: 1)
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        Image(.buttonRectangleDepthFlat)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200)
+                        Text("Start Game")
+                            .font(.custom("Jua-Regular", size: 30))
+                            .foregroundStyle(.white)
+                            .shadow(radius: 3)
+                    }
+                }
+                .padding()
+                
+                Spacer()
+                Spacer()
+            }
+        }
     }
 }
 
@@ -136,5 +303,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    GameSettingsView()
 }
